ff59a65dc682f5cf90adbb259440fbdd29ddde26
1085052159 <1085052159@qq.com>
2022-08-24 00:01:30 +0800

    1. add train 240k config

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
-config=local_configs/segformer/B4/segformer.b4.512x512.cartoon.160k.py
-work_dir="work_dirs/segformer.b4.512x512.cartoon_25cls.160k"
+config=local_configs/segformer/B4/segformer.b4.512x512.cartoon.240k.py
+work_dir="work_dirs/segformer.b4.512x512.cartoon_25cls.240k"
 CUDA_VISIBLE_DEVICES=0,1,2 python -m torch.distributed.launch --nproc_per_node=3 --master_port=9998 tools/train.py $config --launcher pytorch --work-dir $work_dir
 
 #CUDA_VISIBLE_DEVICES=1 python -m torch.distributed.launch --nproc_per_node=1 --master_port=9998 tools/train.py local_configs/segformer/B4/segformer.b4.512x512.cartoon.160k.py --launcher pytorch --work-dir work_dirs/segformer.b4.512x512.cartoon_25cls.160k
--- train_single.sh
+++
@@ -1,5 +0,0 @@
-device_id=1
-config="local_configs/segformer/B4/segformer.b4.512x512.cartoon.160k.py"
-#CUDA_VISIBLE_DEVICES=$device_id python tools/train.py $config
-CUDA_VISIBLE_DEVICES=$device_id python tools/train.py local_configs/segformer/B4/segformer.b4.512x512.cartoon.160k.py
-#CUDA_VISIBLE_DEVICES=2 python tools/train.py local_configs/segformer/B4/segformer.b4.512x512.cartoon.160k.py
